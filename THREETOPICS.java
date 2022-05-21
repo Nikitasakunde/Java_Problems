9b17bec029eaf12127bf30d8562ea850f9666372
Nikitasakunde <105559912+Nikitasakunde@users.noreply.github.com>
2022-05-21 16:52:33 +0530

    Add files via upload

---
+++ THREETOPICS.java
@@ -0,0 +1,25 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int x = sc.nextInt();
+		
+		if((x==a)||(x==b)||(x==c)){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+	}
+}
