f4afba37ed8e8093cd55c92cbfa160dd63f0ba61
Nikita Sakunde <105559912+Nikitasakunde@users.noreply.github.com>
2022-05-29 10:58:56 +0530

    Add files via upload

---
+++ DOLL.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		 
+		while(t-- >0){
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    int arr[] = new int[n];
+		    int count = 0;
+		    
+		    for (int i=0;i<n ;i++ ){
+		        arr[i] = sc.nextInt();
+		        if(k < arr[i]){
+		            count++;
+		        }
+		    } 
+		    System.out.println(count);
+		}
+	}
+}
